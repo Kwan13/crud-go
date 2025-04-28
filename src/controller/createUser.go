d81626cb9bb2fe0b64e50b950f81d5c2a52114bf
Kawan <kawan-silva@outlook.com>
2025-04-28 14:33:12 -0300

    feat: criando objetos do usuário

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kwan13/crud-go/src/configuration/rest_err"
+	"github.com/kwan13/crud-go/src/controller/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+
+		return
+	}
+
+	fmt.Println(userRequest)
 }
